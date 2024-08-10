e6784b9e0e5e26fb5e73626b0c77c2ea1c7f846a
Luiz Serão <luiz.serao@hotmail.com>
2024-08-10 07:41:27 -0300

    section nossas opcoes

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,33 @@
+<div class="bg-inicio"></div>
 <section class="hero-home">
   <h1>Bem-vindo ao Mundo dos Doces Deliciosos! 🍬</h1>
   <p>Satisfaça seu desejo por doces! Aqui, cada mordida é uma explosão de sabor. Nossos doces são feitos com amor e ingredientes de alta qualidade, garantindo que cada pedacinho seja uma experiência inesquecível.</p>
   <a href="<?php echo INCLUDE_PATH ?>cardapio">VER CARDÁPIO</a>
+</section>
+
+<section class="porque-nos-escolher">
+  <h2 class="title">Por Que Escolher Nossos Doces? </h2>
+  <ul>
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/cake.svg" alt="Cake">
+      <h3>Qualidade</h3>
+      <p>Usamos apenas os melhores ingredientes.</p>
+    </li>
+
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/cookie.svg" alt="Cookie">
+      <h3>Sabor Inigualável</h3>
+      <p>Cada doce é uma obra-prima de sabor.</p>
+    </li>
+
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/restaurant.svg" alt="Restaurant">
+      <h3>Atendimento Personalizado</h3>
+      <p>Estamos aqui para tornar sua experiência doce ainda mais especial.</p>
+    </li>
+  </ul>
+</section>
+
+<section class="nossas-opcoes">
+  
 </section>

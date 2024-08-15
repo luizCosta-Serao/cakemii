d3b0fef256760b06faaafc06d07d621c6f00bc99
Luiz Serão <luiz.serao@hotmail.com>
2024-08-15 17:05:41 -0300

    front end da página do carrinho de compras

---
+++ pages/carrinho.php
@@ -0,0 +1,41 @@
+<div class="bg-inicio"></div>
+
+<section class="carrinho">
+  <h1 class="title">Carrinho</h1>
+  <ul>
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/produto-foto.png" alt="">
+      <h2>Bolo de Aniersário</h2>
+      <span class="preco">R$ 199</span>
+      <span class="quantidade">1</span>
+      <a href="">Deletar</a>
+    </li>
+
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/produto-foto.png" alt="">
+      <h2>Bolo de Aniersário</h2>
+      <span class="preco">R$ 199</span>
+      <span class="quantidade">1</span>
+      <a href="">Deletar</a>
+    </li>
+
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/produto-foto.png" alt="">
+      <h2>Bolo de Aniersário</h2>
+      <span class="preco">R$ 199</span>
+      <span class="quantidade">1</span>
+      <a href="">Deletar</a>
+    </li>
+
+    <li>
+      <img src="<?php echo INCLUDE_PATH ?>assets/produto-foto.png" alt="">
+      <h2>Bolo de Aniersário</h2>
+      <span class="preco">R$ 199</span>
+      <span class="quantidade">1</span>
+      <a href="">Deletar</a>
+    </li>
+  </ul>
+
+  <p class="total"><b>Total: </b>R$ 940,00</p>
+  <button class="finalizar">Finalizar Compra</button>
+</section>
